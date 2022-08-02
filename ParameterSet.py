2bab9c0fe11686ce70738ee53cdbb062c0507503
jooooohannes <101879083+jooooohannes@users.noreply.github.com>
2022-08-02 09:35:44 +0200

    made a separate file for the ParameterSet class

---
+++ ParameterSet.py
@@ -0,0 +1,36 @@
+
+
+class ParameterSet:
+    def __init__(self):
+        # parameter initialisation
+        self.longitude = 0
+        self.latitude = 0
+        self.demand = 0
+        self.year = 2020
+        self.centralised = False
+        self.pipeline = False
+        self.max_dist = 0
+
+    # getter functions to be called by run_single_model because the signal can't pass on all the
+    # parameters as arguments
+
+    def get_long(self):
+        return self.longitude
+
+    def get_lat(self):
+        return self.latitude
+
+    def get_demand(self):
+        return self.demand
+
+    def get_year(self):
+        return self.year
+
+    def get_allow_centralised(self):
+        return self.centralised
+
+    def get_allow_pipeline(self):
+        return self.pipeline
+
+    def get_max_pipe_dist(self):
+        return self.max_dist
