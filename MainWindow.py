35cfbaf911120a54b5395682c84a64935ec20324
jooooohannes <101879083+jooooohannes@users.noreply.github.com>
2022-08-02 18:48:05 +0200

    organized widgets in groupboxes

--- MainWindow.py
+++ MainWindow.py
@@ -15,39 +15,93 @@ class UiWindow(QMainWindow):
 
         self.grid = QGridLayout()
 
-        # create labels for the coordinates, latitude and longitude
+        # create groupbox for the coordinates, labels for latitude and longitude
         # as well as input-boxes for latitude and longitude values
-        self.coord_label = QLabel("coordinates :")
+        self.coord_groupbox = QGroupBox("coordinates")
+        self.coord_groupbox.setStyleSheet("QGroupBox { border-style: solid; border-width: 0.5px; "
+                                          "border-radius: 5px; "
+                                          "padding-top: 10px; padding-bottom: 3px } ")
+
+        self.coord_groupbox_layout = QVBoxLayout()
+        self.coord_groupbox.setLayout(self.coord_groupbox_layout)
+
         self.long_label = QLabel("longitude :")
         self.lat_label = QLabel("latitude :")
         self.long_lineedit = QLineEdit()
         self.lat_lineedit = QLineEdit()
 
-        # creates the year label and a combo box with the options of choosing 2020, 2030, 2040, 2050
-        self.year_label = QLabel("year :")
+        # put longitude and latitude labels into horizontal layouts together with their lineedits
+        self.long_hbox = QHBoxLayout()
+        self.long_hbox.addWidget(self.long_label)
+        self.long_hbox.addWidget(self.long_lineedit)
+
+        self.lat_hbox = QHBoxLayout()
+        self.lat_hbox.addWidget(self.lat_label)
+        self.lat_hbox.addWidget(self.lat_lineedit)
+
+        self.coord_groupbox_layout.addLayout(self.lat_hbox)
+        self.coord_groupbox_layout.addLayout(self.long_hbox)
+
+        # creates the year groupbox and a combo box with the options of choosing 2020, 2030, 2040, 2050
+        self.year_groupbox = QGroupBox("year")
+        self.year_groupbox.setStyleSheet("QGroupBox { border-style: solid; border-width: 0.5px; "
+                                         "border-radius: 5px; padding-top: 10px; padding-bottom: 3px }" )
+        self.year_groupbox_layout = QVBoxLayout()
         self.year_combo = QComboBox()
         self.year_combo.addItems(["2020", "2030", "2040", "2050"])
+        self.year_groupbox_layout.addWidget(self.year_combo)
+        self.year_groupbox.setLayout(self.year_groupbox_layout)
+
+        # create a groupbox for demand and central conversion
+        self.demand_conversion_groupbox = QGroupBox()
+        self.demand_conversion_groupbox.setStyleSheet("QGroupBox { border-style: solid; border-width: 0.5px; "
+                                                      "border-radius: 5px; padding-top: 10px; padding-bottom: 3px }")
+        self.demand_conversion_groupbox_layout = QVBoxLayout()
+        self.demand_conversion_groupbox.setLayout(self.demand_conversion_groupbox_layout)
 
         # creates the yearly hydrogen demand label and a spinbox that takes values between 1 and 1 million
-        # the value can be changed in steps of 10 via the arrows
+        # the value can be changed in steps of 10 via the arrows; arrange it in a HBox layout with the label
         self.hhdemand_label = QLabel("yearly hydrogen demand :")
         self.hhdemand_spinbox = QDoubleSpinBox()
         self.hhdemand_spinbox.setRange(0, 1000000)
         self.hhdemand_spinbox.setSingleStep(10)
-
-        # creates a checkbox to decide whether to allow pipelines as transport medium or not
-        self.pipe_checkbox = QCheckBox()
-        self.pipe_checkbox.setText("allow pipelines")
+        self.hhdemand_layout = QHBoxLayout()
+        self.hhdemand_layout.addWidget(self.hhdemand_label)
+        self.hhdemand_layout.addWidget(self.hhdemand_spinbox)
 
         # creates a checkbox to decide whether to allow central conversion facilities
-        self.conversion_checkbox = QCheckBox()
-        self.conversion_checkbox.setText("allow central conversion")
+        self.conversion_checkbox = QCheckBox("allow central conversion")
+
+        self.demand_conversion_groupbox_layout.addLayout(self.hhdemand_layout)
+        self.demand_conversion_groupbox_layout.addWidget(self.conversion_checkbox)
+
+        # create a groupbox for the pipeline related widgets
+        self.pipe_groupbox = QGroupBox("pipelines")
+        self.pipe_groupbox.setStyleSheet("QGroupBox { border-style: solid; border-width: 0.5px; "
+                                         "border-radius: 5px; padding-top: 10px } ")
+        self.pipe_groupbox_layout = QVBoxLayout()
+
+        # creates a checkbox to decide whether to allow pipelines as transport medium or not
+        self.pipe_checkbox = QCheckBox("allow pipelines")
 
         # creates label and input box for
         self.maxpipe_label = QLabel("maximum pipeline length :")
         self.maxpipe_spinbox = QSpinBox()
         self.maxpipe_spinbox.setRange(0, 20000)
         self.maxpipe_spinbox.setSingleStep(10)
+        self.maxpipe_layout = QHBoxLayout()
+        self.maxpipe_layout.addWidget(self.maxpipe_label)
+        self.maxpipe_layout.addWidget(self.maxpipe_spinbox)
+
+        self.pipe_groupbox_layout.addWidget(self.pipe_checkbox)
+        self.pipe_groupbox_layout.addLayout(self.maxpipe_layout)
+        self.pipe_groupbox.setLayout(self.pipe_groupbox_layout)
+
+        # checkable groupbox to contain the monte carlo widgets
+        self.mc_widgets_groupbox = QGroupBox("monte-carlo-simulation")
+        self.mc_widgets_groupbox.setStyleSheet("QGroupBox { border-style: solid; border-width: 0.5px; "
+                                               "border-radius: 5px; padding-top: 10px } ")
+        self.mc_widgets_groupbox_layout = QVBoxLayout()
 
         # creates a checkbox with the option to toggle between single run and monte carlo sim
         self.mc_checkbox = QCheckBox()
@@ -72,53 +126,39 @@ class UiWindow(QMainWindow):
         self.electro_layout.addWidget(self.electro_label)
         self.electro_layout.addWidget(self.electro_combo)
 
-        self.sublayout = QVBoxLayout()
-        self.sublayout.addLayout(self.iter_layout)
-        self.sublayout.addLayout(self.electro_layout)
+        self.mc_widgets_groupbox_layout.addWidget(self.mc_checkbox)
+        self.mc_widgets_groupbox_layout.addLayout(self.iter_layout)
+        self.mc_widgets_groupbox_layout.addLayout(self.electro_layout)
+        self.mc_widgets_groupbox.setLayout(self.mc_widgets_groupbox_layout)
 
         # creates a button to start the model run
         self.run_button = QPushButton("run model")
 
-        # put longitude and latitude labels into horizontal layouts together with their lineedits
-        self.long_hbox = QHBoxLayout()
-        self.long_hbox.addWidget(self.long_label)
-        self.long_hbox.addWidget(self.long_lineedit)
-
-        self.lat_hbox = QHBoxLayout()
-        self.lat_hbox.addWidget(self.lat_label)
-        self.lat_hbox.addWidget(self.lat_lineedit)
-
-        # force the two checkboxes into vertical layout
-        self.allow_vbox = QVBoxLayout()
-        self.allow_vbox.addWidget(self.pipe_checkbox)
-        self.allow_vbox.addWidget(self.conversion_checkbox)
-
         # arranges all widgets in a grid
-        self.grid.addWidget(self.coord_label, 0, 0)
-        self.grid.addLayout(self.long_hbox, 0, 1)
-        self.grid.addLayout(self.lat_hbox, 1, 1)
-        self.grid.addWidget(self.year_label, 3, 0)
-        self.grid.addWidget(self.year_combo, 3, 1)
-        self.grid.addWidget(self.hhdemand_label, 4, 0)
-        self.grid.addWidget(self.hhdemand_spinbox, 4, 1)
-        self.grid.addLayout(self.allow_vbox, 5, 1)
-        self.grid.addWidget(self.maxpipe_label, 6, 0)
-        self.grid.addWidget(self.maxpipe_spinbox, 6, 1)
-        self.grid.addWidget(self.mc_checkbox, 7, 1)
-        self.grid.addLayout(self.sublayout, 8, 1)
-        self.grid.addWidget(self.run_button, 9, 3)
+        self.grid.addWidget(self.coord_groupbox, 0, 0)
+        self.grid.addWidget(self.mc_widgets_groupbox, 0, 1)
+        self.grid.addWidget(self.demand_conversion_groupbox, 1, 0)
+        self.grid.addWidget(self.pipe_groupbox, 2, 0)
+        self.grid.addWidget(self.year_groupbox, 3, 0)
+        self.grid.addWidget(self.run_button, 4, 1)
 
         self.window.setLayout(self.grid)
 
+        # hide optional widgets by default at the start of the program
         self.iter_label.hide()
         self.iter_lineedit.hide()
         self.electro_label.hide()
         self.electro_combo.hide()
+        self.maxpipe_label.hide()
+        self.maxpipe_spinbox.hide()
 
         self.setCentralWidget(self.window)
 
         # connecting the toggling of the monte carlo checkbox to slot, extending the parameter inputs
         self.mc_checkbox.stateChanged.connect(self.on_mc_checkbox)
+
+        # connecting the toggling of the pipeline checkbox to slot
+        self.pipe_checkbox.stateChanged.connect(self.on_pipeline_checkbox)
 
         # instance of parameterSet
         self.parameter_set = ParameterSet.ParameterSet()
@@ -148,6 +188,14 @@ class UiWindow(QMainWindow):
             self.iter_lineedit.hide()
             self.electro_label.hide()
             self.electro_combo.hide()
+
+    def on_pipeline_checkbox(self):
+        if self.pipe_checkbox.isChecked():
+            self.maxpipe_label.show()
+            self.maxpipe_spinbox.show()
+        else:
+            self.maxpipe_label.hide()
+            self.maxpipe_spinbox.hide()
 
     # setter functions for all parameters
     def set_long(self):
