4803aeeba3151e2a97a805f68c8b39d10c7d26a0
jooooohannes <101879083+jooooohannes@users.noreply.github.com>
2022-08-09 12:36:25 +0200

    organized mc_main functions into class MonteCarloComputing similar to ui_library; now also accesses a ParameterSet instance to get the parameters

--- mc_main.py
+++ mc_main.py
@@ -5,88 +5,92 @@ import timeit
 import os
 
 
-def mc_main(end_plant_tuple, h2_demand, year=2021, centralised=True, pipeline=True, max_pipeline_dist=2000,
-            iterations=1000, elec_type='Alkaline'):
-    """Runs a monte carlo simulation of the model. Takes the desired end location [lat, long], the H2 demand (
-    kt/yr), the year, if redistribution is centralised or not, if pipelines are allowed, and the maximum allowed
-    pipeline distance (km) as input. Calculates the minimum of (transport + generation) cost for all possible start
-    locations to determine the cheapest source of renewable H2. """
+class MonteCarloComputing:
+    def __init__(self, parameter_set):
+        self.parameter_set = parameter_set
 
-    df = pd.read_csv('Data/renewables.csv', index_col=0)
+    def run_mc_model(self):
+        latitude = self.parameter_set.get_lat()
+        longitude = self.parameter_set.get_long()
+        end_tuple = (latitude, longitude)  # [lat, long]
+        h2_demand = self.parameter_set.get_demand()
+        year = self.parameter_set.get_year()
+        centralised = self.parameter_set.get_allow_centralised()
+        pipeline = self.parameter_set.get_allow_pipeline()
+        max_pipeline_dist = self.parameter_set.get_max_pipe_dist()
+        iterations = self.parameter_set.get_iterations()
+        elec = self.parameter_set.get_elec_type()
 
-    total_cost_per_kg_h2 = np.zeros((iterations, len(df)))
-    generation_cost_per_kg = np.zeros((iterations, len(df)))
-    solar_cost = np.zeros((iterations, len(df)))
-    wind_cost = np.zeros((iterations, len(df)))
+        et = end_tuple
+        h2 = h2_demand
+        yr = year
 
-    # Define parameters for generation costs
-    year_diff, capex_extra, capex_h2, lifetime_hours, electrolyser_efficiency, elec_opex, other_capex_elec, water_cost, \
-    capex_wind, opex_wind, capex_solar, opex_factor_solar = define_gen_parameters(year, iterations, elec_type)
+        start = timeit.default_timer()
 
-    df, cost_end_nh3, cost_end_lohc, cost_end_h2_liq = initial_geo_calcs(df, end_plant_tuple,
-                                                                         centralised=centralised, pipeline=pipeline,
-                                                                         max_pipeline_dist=max_pipeline_dist)
+        total_cost_per_kg_h2, generation_cost_per_kg_h2, solar_cost, wind_cost = self.mc_main(et, h2,
+                                                                                              yr, centralised, pipeline,
+                                                                                              max_pipeline_dist,
+                                                                                              iterations, elec)
 
-    for i in range(iterations):
-        df = mc_generation_costs(df, h2_demand, year_diff, capex_extra[i], capex_h2[i], lifetime_hours,
-                                 electrolyser_efficiency[i], elec_opex[i],
-                                 other_capex_elec[i], water_cost[i],
-                                 capex_wind[i], opex_wind[i], capex_solar[i], opex_factor_solar[i],
-                                 interest=0.08, full_load_hours=2000)
+        newpath = "Results/mc/" + str(round(et[0])) + ',' + str(round(et[1])) + '__' + str(
+            yr) + '__' + str(h2) + '__' + elec + '__' + str(pipeline) + '__' + str(iterations)
+        if pipeline == True:
+            newpath = newpath + '__Pipe'
+        if centralised == False:
+            newpath = newpath + '__decent'
 
-        df = mc_transport_costs(df, end_plant_tuple, h2_demand, cost_end_nh3, cost_end_lohc, cost_end_h2_liq,
-                                centralised=centralised, pipeline=pipeline,
-                                max_pipeline_dist=max_pipeline_dist)
+        if not os.path.exists(newpath):
+            os.makedirs(newpath)
 
-        df['Total Yearly Cost'] = df['Yearly gen. cost'] + df['Yearly Transport Cost']
-        df['Total Cost per kg H2'] = df['Gen. cost per kg H2'] + df['Transport Cost per kg H2']
+        np.savetxt(newpath + '/' + 'total_cost_per_kg_h2.csv', total_cost_per_kg_h2, delimiter=",")
+        np.savetxt(newpath + '/' + 'generation_cost_per_kg_h2.csv', generation_cost_per_kg_h2,
+                   delimiter=",")
+        np.savetxt(newpath + '/' + 'solar_cost.csv', solar_cost, delimiter=",")
+        np.savetxt(newpath + '/' + 'wind_cost.csv', wind_cost, delimiter=",")
 
-        total_cost_per_kg_h2[i, :] = df['Total Cost per kg H2'].values
-        generation_cost_per_kg[i, :] = df['Gen. cost per kg H2'].values
-        solar_cost[i, :] = df['Elec Cost Solar'].values
-        wind_cost[i, :] = df['Elec Cost Wind'].values
+        # stop timer
+        stop = timeit.default_timer()
+        print('Total Time: ', stop - start)
 
-    return total_cost_per_kg_h2, generation_cost_per_kg, solar_cost, wind_cost
+    def mc_main(self, end_plant_tuple, h2_demand, year=2021, centralised=True, pipeline=True, max_pipeline_dist=2000,
+                iterations=1000, elec_type='alkaline'):
+        """Runs a monte carlo simulation of the model. Takes the desired end location [lat, long], the H2 demand (
+        kt/yr), the year, if redistribution is centralised or not, if pipelines are allowed, and the maximum allowed
+        pipeline distance (km) as input. Calculates the minimum of (transport + generation) cost for all possible start
+        locations to determine the cheapest source of renewable H2. """
 
-def set_parameters(latitude, longitude, demand, year, centralised, pipeline, max_dist, iterations, elec_type):
-    # Define parameters for the main model
-    end_tuple = (latitude, longitude)  # [lat, long]
-    h2_demand = demand  # [kt/yr]
-    year = year
-    centralised = centralised
-    pipeline = pipeline
-    max_pipeline_dist = max_dist
-    iterations = iterations
-    elec_type = elec_type
+        df = pd.read_csv(filepath_or_buffer="Data/renewables.csv", index_col=0)
 
-for et in end_tuple:
-    print('Location: ' + str(et))
-    for h2 in h2_demand:
-        print('H2 Demand: ' + str(h2))
-        for yr in year:
-            print('Year: ' + str(yr))
-            for elec in elec_type:
-                print('Elec type: ' + elec)
+        total_cost_per_kg_h2 = np.zeros((iterations, len(df)))
+        generation_cost_per_kg = np.zeros((iterations, len(df)))
+        solar_cost = np.zeros((iterations, len(df)))
+        wind_cost = np.zeros((iterations, len(df)))
 
-                # start timer
-                start = timeit.default_timer()
+        # Define parameters for generation costs
+        year_diff, capex_extra, capex_h2, lifetime_hours, electrolyser_efficiency, elec_opex, other_capex_elec, water_cost, \
+        capex_wind, opex_wind, capex_solar, opex_factor_solar = define_gen_parameters(year, iterations, elec_type)
 
-                total_cost_per_kg_h2, generation_cost_per_kg_h2, solar_cost, wind_cost = mc_main(et, h2, yr, centralised, pipeline, max_pipeline_dist, iterations, elec)
+        df, cost_end_nh3, cost_end_lohc, cost_end_h2_liq = initial_geo_calcs(df, end_plant_tuple,
+                                                                             centralised=centralised, pipeline=pipeline,
+                                                                             max_pipeline_dist=max_pipeline_dist)
 
-                newpath = "Results/mc/" + str(round(et[0])) + ',' + str(round(et[1])) + '__' + str(yr) + '__' + str(h2) + '__' + elec + '__' + str(pipeline) + '__' + str(iterations)
-                if pipeline == True:
-                    newpath = newpath + '__Pipe'
-                if centralised == False:
-                    newpath = newpath + '__decent'
+        for i in range(iterations):
+            df = mc_generation_costs(df, h2_demand, year_diff, capex_extra[i], capex_h2[i], lifetime_hours,
+                                     electrolyser_efficiency[i], elec_opex[i],
+                                     other_capex_elec[i], water_cost[i],
+                                     capex_wind[i], opex_wind[i], capex_solar[i], opex_factor_solar[i],
+                                     interest=0.08, full_load_hours=2000)
 
-                if not os.path.exists(newpath):
-                    os.makedirs(newpath)
+            df = mc_transport_costs(df, end_plant_tuple, h2_demand, cost_end_nh3, cost_end_lohc, cost_end_h2_liq,
+                                    centralised=centralised, pipeline=pipeline,
+                                    max_pipeline_dist=max_pipeline_dist)
 
-                np.savetxt(newpath + '/' + 'total_cost_per_kg_h2.csv', total_cost_per_kg_h2, delimiter=",")
-                np.savetxt(newpath + '/' + 'generation_cost_per_kg_h2.csv', generation_cost_per_kg_h2, delimiter=",")
-                np.savetxt(newpath + '/' + 'solar_cost.csv', solar_cost, delimiter=",")
-                np.savetxt(newpath + '/' + 'wind_cost.csv', wind_cost, delimiter=",")
+            df['Total Yearly Cost'] = df['Yearly gen. cost'] + df['Yearly Transport Cost']
+            df['Total Cost per kg H2'] = df['Gen. cost per kg H2'] + df['Transport Cost per kg H2']
 
-                # stop timer
-                stop = timeit.default_timer()
-                print('Total Time: ', stop - start)
+            total_cost_per_kg_h2[i, :] = df['Total Cost per kg H2'].values
+            generation_cost_per_kg[i, :] = df['Gen. cost per kg H2'].values
+            solar_cost[i, :] = df['Elec Cost Solar'].values
+            wind_cost[i, :] = df['Elec Cost Wind'].values
+
+        return total_cost_per_kg_h2, generation_cost_per_kg, solar_cost, wind_cost
