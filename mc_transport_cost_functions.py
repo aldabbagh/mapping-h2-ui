63aa22572e3acefb44c26a219f6bb8c67b397f8c
jooooohannes <101879083+jooooohannes@users.noreply.github.com>
2022-10-19 17:08:03 +0200

    updated triangle-function value boundaries

--- mc_transport_cost_functions.py
+++ mc_transport_cost_functions.py
@@ -3,6 +3,8 @@ import numpy as np
 
 
 # Costs by material
+# values taken from non mc-program, boundaries are 90% (lower) and 110% (upper) of the initial value
+# except H2 liquefaction conversion cost where different boundaries were found in the literature
 
 def nh3_costs(pipe_dist=99.57142, ship_dist=1.926, truck_dist=-83.0, convert=True, centralised=True, pipeline=True, max_pipeline_dist=2000):
     """Calculates the transport cost of NH3. Requires as input the distance that NH3 will be piped, shipped and
@@ -10,29 +12,29 @@ def nh3_costs(pipe_dist=99.57142, ship_dist=1.926, truck_dist=-83.0, convert=Tru
     Calculation values are drawn from triangular distributions."""
 
     if convert == True:
-        conversion = np.random.triangular(1.00, 1.02, 1.04)
-        export = np.random.triangular(0.1, 0.11, 0.12)
+        conversion = np.random.triangular(0.918, 1.02, 1.122)
+        export = np.random.triangular(0.099, 0.11, 0.121)
         if centralised:
-            reconversion = np.random.triangular(0.8, 0.85, 0.9)
+            reconversion = np.random.triangular(0.765, 0.85, 0.935)
         else:
-            reconversion = np.random.triangular(1.10, 1.13, 1.16)
+            reconversion = np.random.triangular(1.017, 1.13, 1.243)
     else:
         conversion = 0
         reconversion = 0
         export = 0
 
-    ship = np.random.triangular(2.32E-02, 2.323E-02, 2.326E-2) * np.log(ship_dist) - 1.523E-02
+    ship = np.random.triangular(2.0907E-02, 2.323E-02, 2.5553E-2) * np.log(ship_dist) - 1.523E-02
     if max_pipeline_dist > pipe_dist > 400:
-        pipe = np.random.triangular(0.00065, 0.0007, 0.00075) * pipe_dist - 0.0697
+        pipe = np.random.triangular(0.00063, 0.0007, 0.00077) * pipe_dist - 0.0697
     elif pipe_dist < 400:
-        pipe = np.random.triangular(0.00065, 0.0007, 0.00075) * 400 - 0.0697
+        pipe = np.random.triangular(0.00063, 0.0007, 0.00077) * 400 - 0.0697
     else:
         pipe = np.nan
 
     if pipeline == False:
         pipe = np.nan
 
-    truck = np.random.triangular(0.00075, 0.0008, 0.00085) * truck_dist + 0.0664
+    truck = np.random.triangular(0.00072, 0.0008, 0.00088) * truck_dist + 0.0664
 
     total = conversion + export + ship + pipe + truck + reconversion
 
@@ -44,16 +46,16 @@ def h2_gas_costs(pipe_dist=-102.75, truck_dist=-106.0, pipeline=True, max_pipeli
     trucked. Calculation values are drawn from triangular distributions."""
 
     if max_pipeline_dist > pipe_dist > 400:
-        pipe = np.random.triangular(0.00035, 0.0004, 0.00045) * pipe_dist + 0.0424
+        pipe = np.random.triangular(0.00036, 0.0004, 0.00044) * pipe_dist + 0.0424
     elif pipe_dist < 400:
-        pipe = np.random.triangular(0.00035, 0.0004, 0.00045) * 400 + 0.0424
+        pipe = np.random.triangular(0.00036, 0.0004, 0.00044) * 400 + 0.0424
     else:
         pipe = np.nan
 
     if pipeline == False:
         pipe = np.nan
 
-    truck = np.random.triangular(0.0025, 0.003, 0.0035) * truck_dist + 0.3319
+    truck = np.random.triangular(0.0027, 0.003, 0.0033) * truck_dist + 0.3319
 
     return pipe + truck
 
@@ -64,19 +66,19 @@ def lohc_costs(ship_dist=4.9699, truck_dist=-94.78571, convert=True, centralised
     Calculation values are drawn from triangular distributions."""
 
     if convert == True:
-        conversion = np.random.triangular(0.36, 0.41, 0.46)
-        export = np.random.triangular(0.08, 0.10, 0.12)
+        conversion = np.random.triangular(0.369, 0.41, 0.451)
+        export = np.random.triangular(0.09, 0.1, 0.11)
         if centralised:
-            reconversion = np.random.triangular(1, 1.1, 1.2)
+            reconversion = np.random.triangular(0.99, 1.1, 1.21)
         else:
-            reconversion = np.random.triangular(2.3, 2.35, 2.4)
+            reconversion = np.random.triangular(2.115, 2.35, 2.585)
     else:
         conversion = 0
         reconversion = 0
         export = 0
 
-    ship = np.random.triangular(3.4E-2, 3.404E-02, 3.408E-2) * np.log(ship_dist) - 5.458E-02
-    truck = np.random.triangular(0.001, 0.0014, 0.0018) * truck_dist + 0.1327
+    ship = np.random.triangular(3.0636E-2, 3.404E-02, 3.7444E-2) * np.log(ship_dist) - 5.458E-02
+    truck = np.random.triangular(0.00126, 0.0014, 0.00154) * truck_dist + 0.1327
 
     return conversion + export + ship + truck + reconversion
 
@@ -87,18 +89,18 @@ def h2_liq_costs(ship_dist=0.1915, truck_dist=-22.11666, convert=True, centralis
     Calculation values are drawn from triangular distributions."""
 
     if convert == True:
-        conversion = np.random.triangular(1, 1.03, 1.06)
-        export = np.random.triangular(0.8, 0.88, 0.96)
+        conversion = np.random.triangular(0.92, 1.03, 1.38)
+        export = np.random.triangular(0.792, 0.88, 0.968)
         if centralised:
-            reconversion = np.random.triangular(0.01, 0.02, 0.03)
+            reconversion = np.random.triangular(0.018, 0.02, 0.022)
         else:
-            reconversion = np.random.triangular(0.01, 0.02, 0.03)
+            reconversion = np.random.triangular(0.018, 0.02, 0.022)
     else:
         conversion = 0
         reconversion = 0
         export = 0
 
-    ship = np.random.triangular(0.135, 1.353E-01, 1.356E-1) * np.log(ship_dist) + 2.236E-01
-    truck = np.random.triangular(0.005, 0.006, 0.007) * truck_dist + 0.1327
+    ship = np.random.triangular(0.12177, 1.353E-01, 1.4883E-1) * np.log(ship_dist) + 2.236E-01
+    truck = np.random.triangular(0.0054, 0.006, 0.0066) * truck_dist + 0.1327
 
     return conversion + export + ship + truck + reconversion
